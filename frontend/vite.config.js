2d81b685b9d01ff304d1e03177acb396f4062a79
Gursheen Anand <gursheen@frappe.io>
2024-12-19 20:29:10 +0530

    chore: use correct proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
+import frappeui from 'frappe-ui/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import frappeui from 'frappe-ui/vite'
+
+import { webserver_port } from '../../../sites/common_site_config.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,20 +11,36 @@ export default defineConfig({
 		__VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
 	},
 	plugins: [frappeui(), vue()],
+	server: {
+		port: 8080,
+		proxy: getProxyOptions(),
+	},
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
 		},
 	},
 	build: {
-		outDir: '../slides/public/frontend',
+		outDir: `../${path.basename(path.resolve('..'))}/public/frontend`,
 		emptyOutDir: true,
-		commonjsOptions: {
-			include: [/tailwind.config.js/, /node_modules/],
-		},
-		sourcemap: true,
+		target: 'es2015',
 	},
 	optimizeDeps: {
-		include: ['feather-icons', 'showdown', 'tailwind.config.js', 'engine.io-client'],
+		include: ['frappe-ui > feather-icons', 'showdown', 'engine.io-client'],
 	},
 })
+
+function getProxyOptions() {
+	const port = webserver_port || 8000
+	return {
+		'^/(app|login|api|assets|files|private)': {
+			target: `http://127.0.0.1:${port}`,
+			ws: true,
+			router: function (req) {
+				const site_name = req.headers.host.split(':')[0]
+				console.log(`Proxying ${req.url} to ${site_name}:${port}`)
+				return `http://${site_name}:${port}`
+			},
+		},
+	}
+}
