a4d98bbde4b30721df8936a7cd5ba2b7d248a508
Gursheen Anand <gursheen@frappe.io>
2025-03-31 13:11:28 +0530

    chore: use frappeProxy plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,17 +3,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-import { webserver_port } from '../../../sites/common_site_config.json'
-
 // https://vitejs.dev/config/
 export default defineConfig({
 	define: {
 		__VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
 	},
-	plugins: [frappeui(), vue()],
+	plugins: [
+		frappeui({
+			frappeProxy: true,
+		}),
+		vue(),
+	],
 	server: {
-		port: 8080,
-		proxy: getProxyOptions(),
+		allowedHosts: true,
 	},
 	resolve: {
 		alias: {
@@ -26,21 +28,12 @@ export default defineConfig({
 		target: 'es2015',
 	},
 	optimizeDeps: {
-		include: ['frappe-ui > feather-icons', 'showdown', 'engine.io-client'],
+		include: [
+			'feather-icons',
+			'showdown',
+			'engine.io-client',
+			'tailwind.config.js',
+			'lowlight',
+		],
 	},
 })
-
-function getProxyOptions() {
-	const port = webserver_port || 8000
-	return {
-		'^/(app|login|api|assets|files|private)': {
-			target: `http://127.0.0.1:${port}`,
-			ws: true,
-			router: function (req) {
-				const site_name = req.headers.host.split(':')[0]
-				console.log(`Proxying ${req.url} to ${site_name}:${port}`)
-				return `http://${site_name}:${port}`
-			},
-		},
-	}
-}
