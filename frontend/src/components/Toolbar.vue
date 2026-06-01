3a6466ae500ea3f2d30a37f51447c959d3449fb7
Gursheen Anand <gursheen@frappe.io>
2026-06-01 14:12:31 +0530

    feat: add table insert button

--- frontend/src/components/Toolbar.vue
+++ frontend/src/components/Toolbar.vue
@@ -32,6 +32,12 @@
 
 		<ShapesPopover />
 
+		<Tooltip text="Table" :hover-delay="0.7">
+			<div class="cursor-pointer rounded p-2 hover:bg-gray-100">
+				<Table2 size="16" class="stroke-[1.5]" />
+			</div>
+		</Tooltip>
+
 		<div class="h-6 border-l"></div>
 
 		<Tooltip v-for="option in slideActions" :text="option.label" :hover-delay="0.5">
@@ -50,7 +56,7 @@
 <script setup>
 import { ref } from 'vue'
 
-import { Type, ImagePlus, Trash, Copy, Square } from 'lucide-vue-next'
+import { Type, ImagePlus, Trash, Copy, Square, Table2 } from 'lucide-vue-next'
 
 import { Tooltip, FileUploader, toast } from 'frappe-ui'
 import { isPublicPresentation, presentationId } from '@/stores/presentation'
